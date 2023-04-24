0ead3365310b53ae4c5f88656c50fd703496e794
Matthew <macnemat@gmail.com>
2023-04-24 15:32:27 -0400

    Adjustments to Catch Finance Answer Physics

--- Tahpin_Project/Assets/Scripts/CatchFinances/CatchAnswer.cs
+++ Tahpin_Project/Assets/Scripts/CatchFinances/CatchAnswer.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class CatchAnswer : MonoBehaviour
 {
 
+    [SerializeField] float maxSpeed;
+
     public int slotNumIdentity;
     public string answerText;
     private bool onlyOneHit = false;
+    private Rigidbody2D rb;
     private LevelManagerCatch levelManager;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(rb.velocity.magnitude);
+        if(rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+    }
 
     public void SetLevelManager(LevelManagerCatch levelManager)
     {
