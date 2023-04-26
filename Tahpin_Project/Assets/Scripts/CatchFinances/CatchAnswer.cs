434e0c25934612868984ce833b15d35efb4532d6
Matthew <macnemat@gmail.com>
2023-04-26 18:55:19 -0400

    Blocks move when resting on the side of the basket

--- Tahpin_Project/Assets/Scripts/CatchFinances/CatchAnswer.cs
+++ Tahpin_Project/Assets/Scripts/CatchFinances/CatchAnswer.cs
@@ -11,16 +11,27 @@ public class CatchAnswer : MonoBehaviour
     public string answerText;
     private bool onlyOneHit = false;
     private Rigidbody2D rb;
+    private ConstantForce2D cf;
     private LevelManagerCatch levelManager;
+    public float dir = 0;
+    private bool basketContact;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        cf = GetComponent<ConstantForce2D>();
+        if(transform.position.x < 0)
+        {
+            dir = -1;
+        }
+        else
+        {
+            dir = 1;
+        }
     }
 
     private void Update()
     {
-        Debug.Log(rb.velocity.magnitude);
         if(rb.velocity.magnitude > maxSpeed)
         {
             rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
@@ -34,6 +45,10 @@ public class CatchAnswer : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if(collision.gameObject.layer == 7 || collision.gameObject.CompareTag("FallingAnswer"))
+        {
+            cf.enabled = false;
+        }
         if (slotNumIdentity != 0 && onlyOneHit == false)
         {
             if (collision.gameObject.CompareTag("Bottom"))
@@ -52,4 +67,35 @@ public class CatchAnswer : MonoBehaviour
         }
     }
 
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            if((transform.position.x - collision.transform.position.x) < 0)
+            {
+                dir = 1;
+            }
+            else
+            {
+                dir = -1;
+            }
+            basketContact = true;
+            rb.AddRelativeForce(new Vector2(3 * dir, 0));
+        }
+        else if (collision.gameObject.CompareTag("FallingAnswer") && !basketContact)
+        {
+            CatchAnswer otherAnswer = collision.gameObject.GetComponent<CatchAnswer>();
+            if(dir == otherAnswer.dir)
+            {
+                dir = dir * -1;
+            }
+            rb.AddRelativeForce(new Vector2(3 * dir, 0));
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        cf.enabled = true;
+        basketContact = false;
+    }
 }
